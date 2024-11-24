c60c13f51f8c9db43f3e55d2982082a1933c35f1
saidjon02 <fozilovsaidjon71@gmail.com>
2024-11-24 08:45:52 +0300

    secon commit

---
+++ src/Admin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Admin() {
+  return (
+    <div>
+      <h1>My name is Fozilov Saidjon and Im admin this site</h1>
+    </div>
+  );
+}
+
+export default Admin;
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "boxicons";
+import Home from "./Home";
+import Admin from "./Admin";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
